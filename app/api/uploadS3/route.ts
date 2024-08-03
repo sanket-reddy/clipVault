a565b077ef7b16dee54217ae6713d456782af55e
sanket-reddy <postittom3@gmail.com>
2024-08-03 20:58:55 +0530

    added more file routing in s3 bucket

--- app/api/uploadS3/route.ts
+++ app/api/uploadS3/route.ts
@@ -1,43 +1,44 @@
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { NextResponse,NextRequest } from "next/server";
- export async function POST(req : NextRequest, res: NextResponse){
-
-    const data  = await req.formData()
-    const file : File |undefined = data.get("file")as unknown as File;
-    if(!file){
-        return NextResponse.json({
-            message : "no file found",
-            status : 200
-        })
-    }
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-
-
-    const client = new S3Client({
-        region: "ap-southeast-2",
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
-            secretAccessKey: process.env.AWS_SECRET_aCCESS_KEY ?? ""
-        }
+import { NextResponse, NextRequest } from "next/server";
+export async function POST(req: NextRequest, res: NextResponse) {
+  const data = await req.formData();
+  const file: File | undefined = data.get("file") as unknown as File;
+  const fileName: string = data.get("fileName")  as string;
+  const ContentType: string = data.get("contentType")as  string;
+  const userId : string = data.get("userId")as string;
+  if (!file) {
+    return NextResponse.json({
+      message: "no file found",
+      status: 200,
     });
-    
-     async function putObject(fileName:string,ContentType : string) {
-        const params= {
-            Bucket : "clipvaulttemp",
-            Key : fileName,
-            ContentType
-            
-        }
-        const url = await getSignedUrl(client, new PutObjectCommand(params)); 
-        return url;
-    }
+  }
 
-    let url =await putObject("nextjsObject", "image/jpeg");
-    if(url){
-        return NextResponse.json({message : "the url is recieved ", url,status : 200})
-    }
-    return NextResponse.json({msg : "no url recieved something went wrong"})
+  const client = new S3Client({
+    region: "ap-southeast-2",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+      secretAccessKey: process.env.AWS_SECRET_aCCESS_KEY ?? "",
+    },
+  });
 
- } 
+  async function putObject(fileName: string, ContentType: string) {
+    const params = {
+      Bucket: "clipvaulttemp",
+      Key: userId + fileName,
+      ContentType,
+    };
+    const url = await getSignedUrl(client, new PutObjectCommand(params));
+    return url;
+  }
+
+  let url = await putObject(fileName, ContentType);
+  if (url) {
+    return NextResponse.json({
+      message: "the url is recieved ",
+      url,
+      status: 200,
+    });
+  }
+  return NextResponse.json({ msg: "no url recieved something went wrong" });
+}
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -3,29 +3,44 @@ import Navbar from "@/components/ui/navbar";
 import axios from "axios";
 import Image from "next/image";
 import {  useState } from "react";
+import { useAuth,useUser } from "@clerk/nextjs";
 
 export default function Page(){
+    // const { userId  , sessionId , getToken }  = useAuth();
+    const {isLoaded , isSignedIn, user} = useUser();
+    
+    // console.log("userId : ",userId);
+    // console.log("sessionId : ",sessionId)
+    // console.log("getToken", getToken);
+    console.log("isloading : ",isLoaded);
+    console.log("isSignedIn : ",isSignedIn)
+    
 
     const [file,setFile] = useState<File | undefined>(undefined);
     const [fileUrl, setFileUrl] = useState<string>("");
-    const [url,setUrl] = useState<string>("");    
+    const [fileName, setFileName] = useState<string>("");
+    const [contentType,setContentType] = useState<string>("");
     const fileAdd = (e : React.ChangeEvent<HTMLInputElement>)=>{
         const file = e.target.files?.[0]
         setFile(file);
         if(file){
             let url : string = URL.createObjectURL(file);
             setFileUrl(url);
-            console.log(url);
         }
     }
     const handleSubmit = async ()=>{
+        if(!user){
+            alert("please login !!!");
+        }
+        else{
         if(!file)return null;
-
         const formData = new FormData();
+        formData.append("userId",user.id);
         formData.append("file",file);
+        formData.append("fileName",fileName)
+        formData.append("contentType",contentType);
         let resp =await axios.post("/api/uploadS3",formData);
         let url = resp.data.url;
-        console.log(url);
         try{
             let resp2 = await axios.put(url, file, {
                 headers: {
@@ -37,7 +52,7 @@ export default function Page(){
         catch(error){
             console.log("an error has occured here : ",error);
         }
-        // console.log("file : ",resp.data.file);
+    }
     }
     
     return <div className="mx-4">
@@ -47,7 +62,23 @@ export default function Page(){
          {file && fileUrl ? (<Image src = {fileUrl} alt= "file" width = {200} height={200}></Image>) : (
             <h1>No image</h1>
          )}
-
+        <input
+        onChange={(e)=>{
+            setFileName(e.target.value);
+        }}
+        placeholder="fileName"
+         className=" text-black"
+        ></input>
+        <br></br>
+        <br></br>
+        <input
+        placeholder="content-type"
+        onChange={(e)=>{
+            setContentType(e.target.value);
+        }}
+         className="  text-black"
+        ></input>
+        <br></br>
          <button onClick={handleSubmit}>Send</button>
         </main>
     </div>
