a42c1a9b0c14267eb83c8f12b48d63aa1b5d5e45
sanket-reddy <pathuri.sanketh@gmail.com>
2024-11-09 18:59:10 +0530

    formated user page

--- app/user01/page.tsx
+++ app/user01/page.tsx
@@ -5,6 +5,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export default async function Page (){
     const {userId} = auth();
+    
     async function getPost(){
         "use server"
         if(userId){
@@ -21,12 +22,17 @@ export default async function Page (){
     const objects = await getPost();
     
     return <div>
-        <h1>user01</h1>
+        <div className="border-b flex item-center justify-between p-3 border-white h-[50px]">
+            <h1>File name</h1>
+            <h1>Content Type</h1>
+            <h3>Created At</h3>
+        </div>
         {objects ? <div>{objects.map((item, index)=>(
             <div key = {index}>
-                <div className="flex justify-between w-full">
+                <div className="flex p-3 justify-between w-full">
                     <h1>{item.fileName}</h1>
-                    <h1>{item.contentType}</h1>   
+                    <h1>{item.contentType}</h1>
+                    <h1>{item.createdAt.toLocaleDateString()}</h1>   
                 </div>
             </div>
         ))}</div> : <h1>no user found</h1>}
